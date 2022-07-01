16a5d805595af26b9b7cd4bfa32b5e2f884aa6d0
unknown <hamamhussein10@gmail.com>
2022-07-01 18:15:19 +0300

    v1

--- js/script.js
+++ js/script.js
@@ -25,15 +25,25 @@ $(() => {
 
     let i = 0;
 
-let img = ['/imgs/01.jpg', '/imgs/02.jpg', '/imgs/03.jpg', '/imgs/04.jpg', '/imgs/05.jpg', '/imgs/06.jpg', ];
+
+
+let imgApi = [
+    {img: 'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    {img: 'https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    {img: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    {img: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    {img: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    {img: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'},
+    {img: 'https://images.unsplash.com/photo-1593720213428-28a5b9e94613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'},
+]
 
 
 
 
     setInterval(() =>  {
-let random  = Math.floor(Math.random() * img.length);
+let random  = Math.floor(Math.random() * imgApi.length);
 
-document.querySelector('.card').style.backgroundImage = `url(${img[random]})`
+document.querySelector('.card').style.backgroundImage = `url(${imgApi[random].img})`
 
     }, 4000)
 

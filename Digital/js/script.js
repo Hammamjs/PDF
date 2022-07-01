4787b13700ec3963a4efb1095911074797d20fb3
unknown <hamamhussein10@gmail.com>
2022-07-01 11:18:51 +0300

    PDF Project

---
+++ Digital/js/script.js
@@ -0,0 +1,22 @@
+$(() => {
+    
+
+    $('.header .bar').on('click' , function ()  {
+                $('.nav').css({
+            left : '0px'
+        })
+
+    })
+
+
+
+
+
+
+    $('.nav span').on('click' , function()  {
+        $('.nav').css({
+            left : '-100%'
+        })
+    })
+})
+
